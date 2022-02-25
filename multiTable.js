eb7f2d8dc340f93057144efc2da91326a59b6409
Joao Gabriel Pawlick <JGPawlick@gmail.com>
2022-02-25 09:45:12 -0300

    Entrega
    A tabela ia ficar gigante, dai fiz 2 de dexemplo.

---
+++ multiTable.js
@@ -0,0 +1,31 @@
+/*
+Criar uma fução que receba numero que deseja multiplicar --- OK
+1- criar uma variavel array --- OK
+2- usar looping para contar de 0 até number no eixo X e usar **.push()** ---ArrayVaziu--- .push([]) --- OK
+3- usar while para repetir a ação ---error--- usar for com counter Y --- OK
+4- dar push em cada array com a mutiplica da tabela
+5- rcon
+
+*/
+
+function multiplyTable(number) {
+    let myTable = [];
+
+    for (let counterX = 0; counterX <= number; counterX++) {
+        myTable.push([])
+        //console.log(myTable)
+        for (let counterY = 0; counterY <= number; counterY++) {
+            myTable[counterX].push(counterX * counterY)
+            //console.log(myTable)
+        }
+
+
+        /*while (myTable.push([])) {
+            myTable[counterX].push(counterX * counterX)
+
+            continue
+        }*/
+    }
+    console.table(myTable)
+
+}
